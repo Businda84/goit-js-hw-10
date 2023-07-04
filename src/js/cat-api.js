7809842e6398ed9abffbaec6a84f2d04018c5e03
Businda84 <109301774+Businda84@users.noreply.github.com>
2023-07-04 23:41:42 +0300

    hw 10

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,36 +1,28 @@
-
-API_KEY = 'live_B7QukuIsBSBJWkckmUZ9hvFxh84zQl6CmvCheiUdl52uZVO2YpBxrWK4lvLWUlY6'
+// API_KEY = 'live_B7QukuIsBSBJWkckmUZ9hvFxh84zQl6CmvCheiUdl52uZVO2YpBxrWK4lvLWUlY6'
 const BASE_URL = 'https://api.thecatapi.com/v1/breeds';
-
-
-
-
 
 // повертаємо проміс з данними про кота по ID
 function fetchBreeds() {
-  
   return fetch(BASE_URL, {
     headers: {
       'x-api-key': API_KEY,
     },
   }).then(res => {
-
     if (!res.ok) {
       throw new Error(res.status);
     }
-    return res.json()
- 
-  })
-};
+    return res.json();
+  });
+}
 
 function fetchCatByBreed(breedId) {
-  return fetch(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}&api_key=${API_KEY}`)
-    .then(res => {
+  return fetch(
+    `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}&api_key=${API_KEY}`
+  ).then(res => {
     if (!res.ok) {
-        throw new Error(res.status);
+      throw new Error(res.status);
     }
     return res.json();
-    
-});
-};
+  });
+}
 export { fetchBreeds, fetchCatByBreed };
