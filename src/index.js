dcc869c2f858c4313ff30b840fc84c0627a74788
Businda84 <109301774+Businda84@users.noreply.github.com>
2023-07-04 23:36:31 +0300

    hw 9

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-import SlimSelect from 'slim-select'
-import { fetchBreeds,fetchCatByBreed } from '../src/cat-api';
-import axios from "axios";
-import 'slim-select/dist/slimselect.css'
+import SlimSelect from 'slim-select';
+import { fetchBreeds, fetchCatByBreed } from './js/cat-api';
+// import axios from "axios";
+import 'slim-select/dist/slimselect.css';
 import Notiflix from 'notiflix';
-// 
-axios.defaults.headers.common["x-api-key"] = 'live_B7QukuIsBSBJWkckmUZ9hvFxh84zQl6CmvCheiUdl52uZVO2YpBxrWK4lvLWUlY6';
-const selectBreed = document.querySelector('select.breed-select'); 
+
+//
+// axios.defaults.headers.common["x-api-key"] = 'live_B7QukuIsBSBJWkckmUZ9hvFxh84zQl6CmvCheiUdl52uZVO2YpBxrWK4lvLWUlY6';
+
+const selectBreed = document.querySelector('select.breed-select');
 const catInfo = document.querySelector('.cat-info');
 const loaderEl = document.querySelector('.loader');
 const errorEl = document.querySelector('.error');
@@ -13,51 +15,71 @@ const errorEl = document.querySelector('.error');
 errorEl.style.display = 'none';
 loaderEl.style.display = 'none';
 
-fetchBreeds().then(data => {
-  console.log(data);
-    const options = data.map(({ id, name }) => `<option value='${id}'>${name}</option>`).join('');
-    selectBreed.insertAdjacentHTML('beforeend', options);
-  new SlimSelect({
-    select:'.breed-select',
-   
-    }
-)
-}).catch(err => {
-  errorEl.style.display = 'block'
-  return Notiflix.Notify.failure('Такого котика не знайдено')
-}
-  
-   );
+selectBreed.addEventListener('change', onSelectResult);
 
-
-selectBreed.addEventListener('change', onSelect);
-
-function onSelect(e) {
+function createOptions() {
   loaderEl.style.display = 'block';
-    e.preventDefault();
-    const selectById = e.target.value;
-   console.log(selectById);
-  fetchCatByBreed(selectById)
-    .then(data => {
+  console.log(catInfo);
 
-           markUp(data);
-        })
-    .catch(err => Notiflix.Notify.failure('Котика не знайдено'));
-    };
-
-// створюємо розмітку
-function markUp(item) {
-   const catMarkUp = ({ url, breeds }) => {
-console.log(url);
- `<img class='img-cat' src="${url}" alt="${breeds}"/>
-<h2 class='cat-name'> ${breeds[0].name}</h2>
-<p class='cat-description'>${breeds[0].description}</p>
-<p class='cat-temperament>${breeds[0].temperament}</p>
-  `};
-    const markUpCat = item.map(obj => catMarkUp(obj)).join('');
-  catInfo.innerHTML = markUpCat;
-  loaderEl.style.display = 'none';
+  fetchBreeds()
+    .then(getAllIds)
+    .catch(error => {
+      if (error) {
+        showError();
+      }
+    });
 }
 
+function getAllIds(arr) {
+  for (let i = 0; i < arr.length; i += 1) {
+    let value = arr[i].id;
+    let text = arr[i].name;
 
+    const optionsElement = document.createElement('option');
+    optionsElement.value = value;
+    optionsElement.text = text;
+    selectBreed.appendChild(optionsElement);
+  }
 
+  new SlimSelect({
+    select: '.breed-select',
+  });
+}
+
+function showError() {
+  Notiflix.Report.failure('Котика не знайдено');
+}
+
+createOptions();
+
+//rendering by chosen cat breed
+function onSelectResult() {
+  const breedId = selectedBreed();
+
+  fetchCatByBreed(breedId)
+    .then(renderingCatImg)
+    .catch(error => console.log(error));
+}
+
+function selectedBreed() {
+  const selectedValue = selectBreed.options[selectBreed.selectedIndex];
+  const selectedText = selectedValue.textContent;
+
+  const selectedId = selectedValue.value;
+
+  return selectedId;
+}
+
+function renderingCatImg(arr) {
+  let imgUrl = arr.map(link => link.url);
+  let catDesc = arr.map(desc => desc.breeds[0].description);
+  let catTemp = arr.map(temp => temp.breeds[0].temperament);
+
+  const markup = `<img class="cat-img" src="${imgUrl}" width="300" heigth="300">
+  <div>
+    <p><b>Description:</b> ${catDesc}</p>
+    <p><b>Temperament:</b> ${catTemp}</p>
+  </div>`;
+
+  catInfo.insertAdjacentHTML('beforeend', markup);
+}
--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,6 +1,6 @@
 
 API_KEY = 'live_B7QukuIsBSBJWkckmUZ9hvFxh84zQl6CmvCheiUdl52uZVO2YpBxrWK4lvLWUlY6'
-const BASE_URL = ' https://api.thecatapi.com/v1/';
+const BASE_URL = 'https://api.thecatapi.com/v1/breeds';
 
 
 
@@ -9,7 +9,11 @@ const BASE_URL = ' https://api.thecatapi.com/v1/';
 // повертаємо проміс з данними про кота по ID
 function fetchBreeds() {
   
-  return fetch(`${BASE_URL}breeds?api_key=${API_KEY} `).then(res => {
+  return fetch(BASE_URL, {
+    headers: {
+      'x-api-key': API_KEY,
+    },
+  }).then(res => {
 
     if (!res.ok) {
       throw new Error(res.status);
@@ -18,8 +22,9 @@ function fetchBreeds() {
  
   })
 };
+
 function fetchCatByBreed(breedId) {
-  return fetch(`${BASE_URL}/images/search?breed_ids=${breedId}&api_key=${API_KEY}`)
+  return fetch(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}&api_key=${API_KEY}`)
     .then(res => {
     if (!res.ok) {
         throw new Error(res.status);
